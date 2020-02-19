4d40c64408e2ced927a86cee04d9ca9bed9aa2d2
Sumaer Jolly <sumaerjolly@gmail.com>
2020-02-19 20:43:03 +0530

    generated recipe controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      get 'recipes/create'
+      get 'recipes/show'
+      get 'recipes/destroy'
+    end
+  end
   root 'homepage#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
