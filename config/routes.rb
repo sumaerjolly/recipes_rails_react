d91f4bfd832500ed12a5a8dce762c0c0ed3a68bb
Sumaer Jolly <sumaerjolly@gmail.com>
2020-02-19 21:07:43 +0530

    updated routes for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'recipes/index'
-      get 'recipes/create'
-      get 'recipes/show'
-      get 'recipes/destroy'
+      post 'recipes/create'
+      get '/show/:id', to: 'recipes#show'
+      delete '/destroy/:id', to: 'recipes#destroy'
     end
   end
   root 'homepage#index'
+  get '/*path' => 'homepage#index'
 end
