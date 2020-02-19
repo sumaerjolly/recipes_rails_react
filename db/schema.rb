14db64ac33c0e30a7de5d4f9f6812de1658c7ed5
Sumaer Jolly <sumaerjolly@gmail.com>
2020-02-19 19:57:55 +0530

    added image as a string to be used for src into recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_124837) do
+ActiveRecord::Schema.define(version: 2020_02_19_142604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_02_19_124837) do
     t.text "instruction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
