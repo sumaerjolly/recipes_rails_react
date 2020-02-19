79ab00f4f934abb3f9d78d8b6ef7c62b1cb887b0
Sumaer Jolly <sumaerjolly@gmail.com>
2020-02-19 19:06:59 +0530

    user tests added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations checking' do
+		it 'ensures presence of a username' do 
+			@user1 = User.new(username: "")
+			expect(@user1.save).to eq(false)
+		end
+
+		it 'ensures presence of a username' do 
+			@user2 = User.new(username: "testing")
+			expect(@user2.save).to eq(true)
+		end
+
+		it 'ensure uniqueness of username' do 
+			@user = User.create(username: "unique")
+			@user3 = User.new(username: "unique")
+			expect(@user3.save).to eq(false)
+		end
+
+		it 'the uniqueness isnt case sensitivie' do 
+			@test = User.create(username: "ram")
+			@test1 = User.new(username: "RAM")
+			expect(@test1.save).to eq(false)
+		end
+	end
 end
