2dd8b39dd99cc0d862b8e226ed165dd9bded3a7e
Sumaer Jolly <sumaerjolly@gmail.com>
2020-02-19 20:14:13 +0530

    added tests for other favourite and recipe model

--- spec/models/favourite_spec.rb
+++ spec/models/favourite_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Favourite, type: :model do
+	describe 'validations checking' do
+		it 'ensures that a user can add a recipe to favourites only once' do
+			@user = User.create(username: "microverse")
+			@recipe = Recipe.new(name: "test name", ingredients: "testing ingredient", instruction: "testing again")
+			@favourite = Favourite.create(user: @user, recipe: @recipe)
+			@favourite2 = Favourite.new(user: @user, recipe: @recipe)
+			expect(@favourite2.save).to eq(false)
+		end 
+	end	
+	
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
+  describe 'validations checking' do
+		it 'ensures presence of a name' do 
+			@recipe = Recipe.new(name: "", ingredients: "testing", instruction: "testing again")
+			expect(@recipe.save).to eq(false)
+		end
+		it 'ensures presence of ingredients' do 
+			@recipe1 = Recipe.new(name: "wohoo", ingredients: "", instruction: "testing again")
+			expect(@recipe1.save).to eq(false)
+		end
+
+		it 'ensures presence of instrutction' do 
+			@recipe2 = Recipe.new(name: "wohoo", ingredients: "testing", instruction: "")
+			expect(@recipe2.save).to eq(false)
+		end
+	end
 end
