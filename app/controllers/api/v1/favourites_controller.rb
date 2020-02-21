6af6c9bc73d9e939f7fb5088b0f2ba141a5966cc
Sumaer Jolly <sumaerjolly@gmail.com>
2020-02-21 16:40:27 +0530

    controller tests added

--- app/controllers/api/v1/favourites_controller.rb
+++ app/controllers/api/v1/favourites_controller.rb
@@ -2,20 +2,34 @@ class Api::V1::FavouritesController < ApplicationController
   include CurrentUserConcern
   
   def index
-    favourites = @current_user.favourites
-    render json: favourites
+    if @current_user 
+      favourites = @current_user.favourites
+      render json: favourites
+    else 
+      render json: {message: "You need to be signed in"}, status: 500
+    end
   end
 
   def create
-    favourite = @current_user.favourites.build(recipe_id: params[:id])
-    if favourite
-      render json: favourite
+    if @current_user
+      favourite = @current_user.favourites.build(recipe_id: params[:id])
+      if favourite.save
+        render json: favourite
+      else
+        render json: favourite.errors 
+      end
     else
-      render json: favourite.errors
+      render json: {message: "You need to be signed in"}, status: 500
     end
   end
 
   def destroy
     favourite = @current_user.favourites.find_by(recipe_id: params[:id])
+    if favourite
+      favourite.destroy
+      render json: { message: 'Removed from favourites' }
+    else
+      render json: {message:'There was an error' }
+    end
   end
 end
