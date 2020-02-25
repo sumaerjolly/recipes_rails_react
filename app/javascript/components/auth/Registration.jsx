2111782bd3b3f1be37e482f07dcf3f8334d73458
Sumaer Jolly <sumaerjolly@gmail.com>
2020-02-25 22:22:43 +0530

    sign up form styling completed

--- app/javascript/components/auth/Registration.jsx
+++ app/javascript/components/auth/Registration.jsx
@@ -61,7 +61,7 @@ class Registration extends Component {
     if (this.state.registrationErrors) {
       return (
         <div
-          className="alert alert-warning alert-dismissible fade show"
+          className="alert alert-warning alert-dismissible fade show mt-0 mb-0"
           role="alert"
         >
           Please Recheck your entered credentials
@@ -82,33 +82,63 @@ class Registration extends Component {
     return (
       <div>
         {this.errorAlert()}
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            name="username"
-            placeholder="username"
-            value={this.state.username}
-            onChange={this.handleChange}
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={this.handleChange}
-            required
-          />
-          <input
-            type="password"
-            name="password_confirmation"
-            placeholder="Password Confirmation"
-            value={this.state.password_confirmation}
-            onChange={this.handleChange}
-            required
-          />
-          <button type="submit">Register</button>
-        </form>
+        <section id="cover" className="min-vh-100">
+          <div id="cover-caption">
+            <div className="container">
+              <div className="row text-white">
+                <div className="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4">
+                  <h3 className="display-4 py-2 text-truncate">Sign Up</h3>
+                  <div className="px-2">
+                    <form
+                      onSubmit={this.handleSubmit}
+                      className="justify-content-center"
+                    >
+                      <div className="form-group">
+                        <label className="sr-only">Username</label>
+                        <input
+                          className="form-control"
+                          type="text"
+                          name="username"
+                          placeholder="username"
+                          value={this.state.username}
+                          onChange={this.handleChange}
+                          required
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label className="sr-only">Password</label>
+                        <input
+                          className="form-control"
+                          type="password"
+                          name="password"
+                          placeholder="Password"
+                          value={this.state.password}
+                          onChange={this.handleChange}
+                          required
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label className="sr-only">Password Confirmation</label>
+                        <input
+                          className="form-control"
+                          type="password"
+                          name="password_confirmation"
+                          placeholder="Password Confirmation"
+                          value={this.state.password_confirmation}
+                          onChange={this.handleChange}
+                          required
+                        />
+                      </div>
+                      <button type="submit" className="btn-custom">
+                        Sign Up
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
