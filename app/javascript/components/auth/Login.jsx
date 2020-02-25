25dec4921d0a5153f93561d7b3253bcafaabd393
Sumaer Jolly <sumaerjolly@gmail.com>
2020-02-25 17:33:01 +0530

    added bootstrap alert when your credentials are incorrect while logging in

--- app/javascript/components/auth/Login.jsx
+++ app/javascript/components/auth/Login.jsx
@@ -12,6 +12,7 @@ class Login extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+    this.errorAlert = this.errorAlert.bind(this);
   }
 
   handleSuccessfulAuth(data) {
@@ -22,7 +23,7 @@ class Login extends Component {
   handleSubmit(e) {
     axios
       .post(
-        'http://localhost:3000/sessions',
+        '/sessions',
         {
           user: {
             username: this.state.username,
@@ -36,6 +37,11 @@ class Login extends Component {
       .then(response => {
         if (response.data.logged_in) {
           this.handleSuccessfulAuth(response.data);
+        } else if (response.data.status === 401) {
+          this.setState({
+            ...this.state,
+            loginErrors: true
+          });
         }
       })
       .catch(error => {
@@ -50,9 +56,31 @@ class Login extends Component {
     });
   }
 
+  errorAlert() {
+    if (this.state.loginErrors) {
+      return (
+        <div
+          className="alert alert-warning alert-dismissible fade show"
+          role="alert"
+        >
+          Please Recheck your login Credentials
+          <button
+            type="button"
+            className="close"
+            data-dismiss="alert"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div>
+        {this.errorAlert()}
         <form onSubmit={this.handleSubmit}>
           <input
             type="text"
